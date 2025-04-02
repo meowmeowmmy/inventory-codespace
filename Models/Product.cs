9bc26bf37370e15594836f8d71c4fc45d34b14e0
meowmeowmmy <120554954+meowmeowmmy@users.noreply.github.com>
2025-04-02 15:27:27 +0300

    Add files via upload

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,10 +3,10 @@ namespace InventoryCSharp.Models
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int Quantity { get; set; }
         public decimal PriceWithoutVAT { get; set; }
         public decimal PriceWithVAT { get; set; }
-        public string Barcode { get; set; }
+        public string Barcode { get; set; } = string.Empty;
     }
 }
